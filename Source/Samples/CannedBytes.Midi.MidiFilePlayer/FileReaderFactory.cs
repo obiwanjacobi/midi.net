3f1adf3879f40dad20a5b8f4adb469d0969f52ef
Marc Jacobi <obiwanjacobi@hotmail.com>
2012-12-10 14:19:11 +0000

    Midi File Player sample app. Timing for single notes mode is not done.

---
+++ Source/Samples/CannedBytes.Midi.MidiFilePlayer/FileReaderFactory.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.Composition.Hosting;
+using CannedBytes.ComponentModel.Composition;
+using CannedBytes.Media.IO;
+using CannedBytes.Media.IO.ChunkTypes.Midi;
+using CannedBytes.Media.IO.Services;
+
+namespace CannedBytes.Midi.MidiFilePlayer
+{
+    internal class FileReaderFactory
+    {
+        public static FileChunkReader CreateReader(string filePath)
+        {
+            var context = CreateFileContextForReading(filePath);
+
+            var reader = context.CompositionContainer.CreateFileChunkReader();
+
+            return reader;
+        }
+
+        public static ChunkFileContext CreateFileContextForReading(string filePath)
+        {
+            var context = ChunkFileContext.OpenFrom(filePath);
+
+            context.CompositionContainer = CreateCompositionContextForReading();
+
+            return context;
+        }
+
+        public static CompositionContainer CreateCompositionContextForReading()
+        {
+            var factory = new CompositionContainerFactory();
+
+            factory.AddMarkedTypesInAssembly(null, typeof(IFileChunkHandler));
+            // add midi exports
+            factory.AddMarkedTypesInAssembly(typeof(MTrkChunkHandler).Assembly, typeof(IFileChunkHandler));
+
+            // note that Midi files use big endian.
+            // and the chunks are not aligned.
+            factory.AddTypes(
+                typeof(BigEndianNumberReader),
+                typeof(SizePrefixedStringReader),
+                typeof(ChunkTypeFactory),
+                typeof(FileChunkHandlerManager));
+
+            var container = factory.CreateNew();
+
+            var chunkFactory = container.GetService<ChunkTypeFactory>();
+            // add midi chunks
+            chunkFactory.AddChunksFrom(typeof(MTrkChunkHandler).Assembly, true);
+
+            return container;
+        }
+    }
+}
---
+++ Source/Samples/CannedBytes.Midi.MidiFilePlayer/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CannedBytes.Midi.IO;
+using CannedBytes.Midi.Message;
+
+namespace CannedBytes.Midi.MidiFilePlayer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string midiFileName = null;
+            int outPortId = 0;
+
+            if (args.Length > 0)
+            {
+                midiFileName = args[0];
+            }
+
+            if (args.Length > 1)
+            {
+                outPortId = Int16.Parse(args[1]);
+            }
+
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Usage: MidiFilePlayer 'C:\\Folder\\MidiFile.mid' [MidiOutPortId]");
+                return;
+            }
+
+            Console.WriteLine("Reading midi file: " + midiFileName);
+            var fileData = ReadMidiFile(midiFileName);
+
+            IEnumerable<MidiFileEvent> notes = null;
+
+            // merge all track notes and filter out sysex and meta events
+            foreach (var track in fileData.Tracks)
+            {
+                if (notes == null)
+                {
+                    notes = from note in track.Events
+                            where !(note.Message is MidiLongMessage)
+                            select note;
+                }
+                else
+                {
+                    notes = (from note in track.Events
+                             where !(note.Message is MidiLongMessage)
+                             select note).Union(notes);
+                }
+            }
+
+            // order track notes by absolute-time.
+            notes = from note in notes
+                    orderby note.AbsoluteTime
+                    select note;
+
+            WriteHeaderInfoToConsole(fileData.Header);
+
+            var caps = MidiOutPort.GetPortCapabilities(outPortId);
+            MidiOutPortBase outPort = null;
+
+            if ((caps.Support & MidiOutPortCapsSupport.Stream) == 0)
+            {
+                outPort = ProcessIndividual(outPortId, fileData, notes, caps);
+            }
+            else
+            {
+                outPort = ProcessStreaming(outPortId, fileData, notes, caps);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+
+            outPort.Close();
+        }
+
+        private static MidiOutPortBase ProcessIndividual(int outPortId, MidiFileData fileData,
+            IEnumerable<MidiFileEvent> notes, MidiOutPortCaps caps)
+        {
+            var outPort = new MidiOutPort();
+            outPort.Open(outPortId);
+
+            Console.WriteLine(String.Format("Midi Out Port '{0}' is now open.", caps.Name));
+
+            // TODO: timing
+            foreach (var note in notes)
+            {
+                var shortMsg = note.Message as MidiShortMessage;
+
+                if (shortMsg != null)
+                {
+                    outPort.ShortData(shortMsg.Data);
+                }
+            }
+
+            return outPort;
+        }
+
+        private static MidiOutPortBase ProcessStreaming(int outPortId, MidiFileData fileData,
+            IEnumerable<MidiFileEvent> notes, MidiOutPortCaps caps)
+        {
+            var outPort = new MidiOutStreamPort();
+            outPort.Open(outPortId);
+            outPort.MidiBufferManager.Initialize(10, 1024);
+            outPort.TimeDivision = fileData.Header.TimeDivision;
+
+            Console.WriteLine(String.Format("Midi Out Port '{0}' is now open.", caps.Name));
+
+            MidiMessageOutStreamWriter writer = null;
+            MidiBufferStream buffer = null;
+
+            foreach (var note in notes)
+            {
+                if (writer == null)
+                {
+                    buffer = outPort.MidiBufferManager.Retrieve();
+                    writer = new MidiMessageOutStreamWriter(buffer);
+                }
+
+                if (writer.CanWrite(note.Message))
+                {
+                    writer.Write(note.Message, (int)note.DeltaTime);
+                }
+                else
+                {
+                    outPort.LongData(buffer);
+                    writer = null;
+
+                    Console.WriteLine("Buffer sent...");
+
+                    if (!outPort.HasStatus(MidiPortStatus.Started))
+                    {
+                        outPort.Restart();
+                    }
+                }
+            }
+
+            return outPort;
+        }
+
+        private static void WriteHeaderInfoToConsole(MThdChunk mThdChunk)
+        {
+            Console.WriteLine("Number of tracks: " + mThdChunk.NumberOfTracks);
+            Console.WriteLine("Number of format: " + mThdChunk.Format);
+            Console.WriteLine("Number of time division: " + mThdChunk.TimeDivision);
+        }
+
+        internal static MidiFileData ReadMidiFile(string filePath)
+        {
+            var data = new MidiFileData();
+            var reader = FileReaderFactory.CreateReader(filePath);
+
+            data.Header = reader.ReadNextChunk() as MThdChunk;
+
+            var tracks = new List<MTrkChunk>();
+
+            for (int i = 0; i < data.Header.NumberOfTracks; i++)
+            {
+                var track = reader.ReadNextChunk() as MTrkChunk;
+
+                if (track != null)
+                {
+                    tracks.Add(track);
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("Track '{0}' was not read successfully.", i + 1));
+                }
+            }
+
+            data.Tracks = tracks;
+            return data;
+        }
+    }
+}
