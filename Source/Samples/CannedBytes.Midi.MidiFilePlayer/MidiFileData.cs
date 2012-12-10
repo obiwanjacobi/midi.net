cc7a78cf3a8c797eeebf2505c739d06d8e2a4869
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2012-12-10 14:19:11 +0000

    Midi File Player sample app. Timing for single notes mode is not done.

---
+++ Source/Samples/CannedBytes.Midi.MidiFilePlayer/MidiFileData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CannedBytes.Midi.IO;
+
+namespace CannedBytes.Midi.MidiFilePlayer
+{
+    class MidiFileData
+    {
+        public MThdChunk Header;
+        public IEnumerable<MTrkChunk> Tracks;
+    }
+}
