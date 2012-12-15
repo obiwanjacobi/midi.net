f25ce75414fadae445eb218b8a39d2a053d75832
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2012-12-15 21:24:49 +0000

    Starting a SysEx utility sample application.

---
+++ Source/Samples/CannedBytes.Midi.SysExUtil/Midi/MidiSysExReceiver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CannedBytes.Midi.Components;
+
+namespace CannedBytes.Midi.SysExUtil.Midi
+{
+    class MidiSysExReceiver : IMidiDataReceiver
+    {
+        private AppData appData;
+        private MidiInPort inPort;
+        private MidiInPortChainManager chainMgr;
+
+        public MidiSysExReceiver(AppData appData)
+        {
+            this.appData = appData;
+            this.inPort = new MidiInPort();
+            this.chainMgr = new MidiInPortChainManager(this.inPort);
+
+            this.chainMgr.Add(this);
+
+            this.inPort.BufferManager.Initialize(10, 1024);
+        }
+
+        public void Start(int portId)
+        {
+            this.inPort.Open(portId);
+            this.inPort.Start();
+        }
+
+        public void Stop()
+        {
+            this.inPort.Stop();
+            this.inPort.Close();
+        }
+
+        private void ScheduleAddBuffer(MidiSysExBuffer buffer)
+        {
+            this.appData.Dispatcher.Invoke(new Action(() => DispatchedAddBuffer(buffer) ));
+        }
+
+        private void DispatchedAddBuffer(MidiSysExBuffer buffer)
+        {
+            this.appData.SysExBuffers.Add(buffer);
+        }
+
+        #region IMidiDataReceiver Members
+
+        public void LongData(MidiBufferStream buffer, long timestamp)
+        {
+            var sysExBuffer = MidiSysExBuffer.From(buffer);
+
+            ScheduleAddBuffer(sysExBuffer);
+        }
+
+        public void ShortData(int data, long timestamp)
+        {
+            // no op
+        }
+
+        #endregion
+    }
+}
