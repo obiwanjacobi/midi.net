f1248c4a02b4891d9f13f7df8ac23562e35d0742
Marc Jacobi <marc.jacobi@macaw.nl>
2020-06-28 10:03:54 +0200

    xml

---
+++ Source/Code/CannedBytes.Midi.Xml/GenerateCode.cmd
@@ -0,0 +1,5 @@
+::CALL "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
+xsd MIDIEvents10.xsd MIDIDeviceTypes10.xsd MIDINameDocument10.xsd /c /n:CannedBytes.Midi.Xml /edb /order
+
+pause
