4d21cdf697a5149403323ca985bbda90919812c9
Marc Jacobi <marc.jacobi@macaw.nl>
2020-06-29 11:37:57 +0200

    fix CannedBytes dep and update samples

--- Source/Code/push-nuget.cmd
+++ Source/Code/push-nuget.cmd
@@ -1,8 +1,8 @@
 msbuild CannedBytes.Midi.sln /t:Clean /p:Configuration=Release
 msbuild CannedBytes.Midi.sln /t:Build /p:Configuration=Release
 
-nuget push CannedBytes.Midi\bin\Release\CannedBytes.Midi.2.0.1.nupkg -src https://api.nuget.org/v3/index.json
-nuget push CannedBytes.Midi.Components\bin\Release\CannedBytes.Midi.Components.2.0.1.nupkg -src https://api.nuget.org/v3/index.json
-nuget push CannedBytes.Midi.IO\bin\Release\CannedBytes.Midi.IO.2.0.1.nupkg -src https://api.nuget.org/v3/index.json
-nuget push CannedBytes.Midi.Message\bin\Release\CannedBytes.Midi.Message.2.0.1.nupkg -src https://api.nuget.org/v3/index.json
-nuget push CannedBytes.Midi.Xml\bin\Release\CannedBytes.Midi.Xml.2.0.1.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Midi\bin\Release\CannedBytes.Midi.2.0.2.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Midi.Components\bin\Release\CannedBytes.Midi.Components.2.0.2.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Midi.IO\bin\Release\CannedBytes.Midi.IO.2.0.2.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Midi.Message\bin\Release\CannedBytes.Midi.Message.2.0.2.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Midi.Xml\bin\Release\CannedBytes.Midi.Xml.2.0.2.nupkg -src https://api.nuget.org/v3/index.json
